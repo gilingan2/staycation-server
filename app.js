7b89d35b510c9bdca9b1d93f20bee0a73ef539bd
gilingan2 <99949737+gilingan2@users.noreply.github.com>
2023-12-07 11:17:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
+var http = require('http')
+var debug = require('debug')('server-staycation:server')
+
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 const methodOverride = require('method-override')
@@ -75,4 +78,59 @@ app.use(function (err, req, res, next) {
   res.render('error')
 })
 
+var port = normalizePort(process.env.PORT || '3000')
+var server = http.createServer(app)
+
+app.set('port', port)
+server.listen(port, () => console.log(`server berjalan pada port ${port}`))
+server.on('error', onError)
+server.on('listening', onListening)
+
+function normalizePort(val) {
+  var port = parseInt(val, 10)
+
+  if (isNaN(port)) {
+    // named pipe
+    return val
+  }
+
+  if (port >= 0) {
+    // port number
+    return port
+  }
+
+  return false
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
+    default:
+      throw error
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address()
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+  debug('Listening on ' + bind)
+}
 module.exports = app
